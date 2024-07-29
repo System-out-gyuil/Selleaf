a8c099641229944209b408a4a4ab902f27edec9a
System-out-gyuil <a46884334@gmail.com>
2024-07-29 14:41:50 +0900

    auth

--- selleaf/settings.py
+++ selleaf/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xrbh@fs#!56dnm^+1ld6l5n5t7n)1d@6t(@r3a&2!_x1j8r*$r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['3.39.102.97', '127.0.0.1', 'localhost', 'selleaf.online']
 
@@ -66,7 +66,7 @@ INSTALLED_APPS = [
     'rest_framework'
 ]
 
-SITE_ID = 9
+SITE_ID = 4
 
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_LOGOUT_ON_GET = True
